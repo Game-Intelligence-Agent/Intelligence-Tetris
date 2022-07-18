c5deb3e7841db694ee9edeafbb288902aac7e904
SSSKJ <438841268@qq.com>
2022-07-19 00:06:15 +0800

    use .yaml for hyper parameters, add hyper_loader for loading

--- Models/Parameters/__init__.py
+++ Models/Parameters/__init__.py
@@ -1,1 +1,8 @@
 from .hyper import *
+import yaml
+
+def hyper_loader(hyper_name = 'gnn_hyper', hyper_path = './Models/Parameters/hyper'):
+
+    file = open(f'{hyper_path}/{hyper_name}.yaml')
+    return yaml.load(file.read())
+
